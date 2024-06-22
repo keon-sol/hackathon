5efc8f74c9af88611cfd8d3dc43e50161ef32fa9
sid <sidbanerjee@berkeley.edu>
2024-06-22 16:48:56 -0700

    front sends to back but can't receive

--- flask-api/api.py
+++ flask-api/api.py
@@ -1,17 +1,16 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, Response
 from flask_cors import CORS, cross_origin
 import sys
 
 app = Flask(__name__)
 
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
 @app.route('/analyze', methods=['POST'])
-@cross_origin()
 def analyze_text():
     print("hello", file=sys.stderr)
     content = request.json
