1322654ed2b61418d8bab9f3d1cd13b925470df2
Jonathan Camacho <jonathan.hsl@berkeley.edu>
2024-06-22 12:38:45 -0700

    started making the model

---
+++ src/FakeNewsModel.py
@@ -0,0 +1,94 @@
+import pandas as pd 
+import numpy as np 
+import seaborn as sns
+import matplotlib.pyplot as plt 
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+import re
+import string
+
+#imports data on what news is fake and what news is real
+data_fake = pd.read_csv('Fake.csv')
+data_true = pd.read_csv('True.csv')
+data_fake.head()
+data_true.head()
+
+#classifying fake and real news
+data_fake["class"] = 0
+data_true['class'] = 1
+
+#checks number of rows and columns in the data set
+data_fake.shape, data_true.shape
+
+#manually tests data (?)
+data_fake_manual_testing = data_fake.tail(10)
+for i in range(23480, 23470, -1):
+    data_fake.drop([i], axis = 0, inplace = True)
+data_true_manual_testing = data_true.tail(10)
+for i in range(21416, 21406, -1):
+    data_true.drop([i], axis = 0, inplace = True)
+
+#classifies data set
+data_fake_manual_testing['class'] = 0
+data_true_manual_testing['class'] = 1
+
+#merges the fake and true news data sets into one
+data_merge = pd.concat([data_fake, data_true], axis = 0)
+data_merge.head(10)
+
+#gets rid of irrelevant columns
+data = data_merge.drop(['title', 'subject', 'date'], axis = 1)
+
+#cleaning text
+def wordopt(text):
+    text = text.lower()
+    text = re.sub('\[.*?\]', '', text)
+    text = re.sub("\\W", " ", text)
+    text = re.sub('https?://\S+|www\.\S+', '', text)
+    text = re.sub('<.*?>+', '', text)
+    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+    text = re.sub('\n', '', text)
+    text = re.sub('\w*\d\w*', '', text)
+    return text
+
+#applying function to text column and assigning x/y (?)
+data['text'] = data['text'].apply(wordopt)
+x = data['text']
+y = data['class']
+
+#Defining Training and Testing Data and Splitting Them Into &5 -25 Percent Ratio (?)
+x_train, x_test, y_train, t_test = train_test_split(x, y, test_size = 0.25)
+
+#putting raw data into a matrix
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+vectorization = TfidfVectorizer()
+xv_train = vectorization.fit_transform(x_train)
+xv_test = vectorization.transform(x_test)
+
+#creaing regression model
+from sklearn.linear_model import LogisticRegression
+
+LR = LogisticRegression()
+LR.fit(xv_train, y_train)
+
+#checking model accuracy and classification report
+pred_lr = LR.predict(xv_test)
+LR.score(xv_test, y_test)
+print(classification_report(y_test, pred_lr))
+
+#creating decision tree classified model
+from sklearn.tree import DecisionTreeClassifier
+
+DT = DecisionTreeClassifier()
+DT.fit(xv_train, y_train)
+
+#checking model accuracy and classification report
+pred_dt = DT.predict(xv_test)
+DT.score(xv_test, y_test)
+print(classification_report(y_test, pred_dt))
+
+
+news = str(input())
+manual_testing(news)
